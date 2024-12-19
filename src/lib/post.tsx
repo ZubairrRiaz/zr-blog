4f45bfd151b128b2ee3af5881bb41e1a9b0ae31b
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-12-19 20:58:27 +0500

    Add files via upload

--- src/lib/post.tsx
+++ src/lib/post.tsx
@@ -2,27 +2,27 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import path from 'path';
 
-
+interface Post {
+  slug: string;
+  content: string;
+  [key: string]: any; // Allow additional fields from the frontmatter
+}
 
 const postDir = path.join(process.cwd(), 'src/posts');
 
-
-export const getAllPosts = () => {
+export const getAllPosts = (): Post[] => {
     const fileNames = fs.readdirSync(postDir);
     return fileNames.map((fileName) => {
-        const slug = fileName.replace(/\.md$/,'');
-        const filePath = path.join(postDir,fileName);
+        const slug = fileName.replace(/\.md$/, ''); // Remove the `.md` extension to get the slug
+        const filePath = path.join(postDir, fileName);
         const fileContent = fs.readFileSync(filePath, 'utf-8');
-
-        const {content, data} = matter(fileContent);
-      
-
-
+        
+        const { content, data } = matter(fileContent); // `data` is the frontmatter
+        
         return {
             slug,
             content,
             ...data,
-
-        }
-    })
-}
+        };
+    });
+};
