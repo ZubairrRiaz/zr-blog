94764672e04b5484b99338bb0f7668719780fcb8
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-12-19 21:29:15 +0500

    Add files via upload

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -1,28 +1,16 @@
 import { getAllPosts } from "@/lib/post";
 import Link from "next/link";
 
-// Define the Post interface
-interface Post {
-  slug: string;
-  title: string;
-  author: string;
-  date: string;
-  text: string;
-  [key: string]: any; // Allow for additional fields if necessary
-}
 
-// Define the props expected in the component
-interface ContentProps {
-  posts: Post[];
-}
-
-export default function Content({ posts }: ContentProps) {
+export default function Content() {
+  const posts = getAllPosts();
   return (
     <div>
+
       <div id="blog" className="p-8 font-[family-name:var(--font-geist-sans)] mt-14 sm:mt-36 bg-white">
         <h1 className="text-3xl font-bold text-center mb-8 text-gray-500">Available Blogs</h1>
         <div className="flex flex-col sm:flex-wrap sm:flex-row justify-center items-center gap-8">
-          {posts.map((post: Post, index: number) => (
+          {posts.map((post: any, index:number) => (
             <div
               key={index}
               className="bg-white text-center shadow-lg rounded-lg p-6 w-full sm:w-1/3 transform transition duration-500 hover:scale-105"
@@ -35,24 +23,15 @@ export default function Content({ posts }: ContentProps) {
                   href={`/posts/${post.slug}`}
                   className="bg-blue-700 text-white p-2 rounded-md font-bold"
                 >
-                  {post.text}
+                  {" "}
+                  {post.text}{" "}
                 </Link>
               </div>
             </div>
           ))}
         </div>
       </div>
+  
     </div>
   );
-}
-
-// This function runs at build time and fetches the posts
-export async function getStaticProps() {
-  const posts = await getAllPosts();  // Fetch the posts asynchronously
-
-  return {
-    props: {
-      posts,  // Pass the posts to the component as props
-    },
-  };
 }
