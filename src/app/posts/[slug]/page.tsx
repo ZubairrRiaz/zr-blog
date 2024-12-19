7ce9c5b1d035146f65e4bb08c0ed8ed12182368d
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-12-19 21:12:51 +0500

    Add files via upload

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,31 +1,37 @@
-import { getAllPosts } from "@/lib/post"
+import { getAllPosts } from "@/lib/post";
 import { notFound } from "next/navigation";
 import MarkdownIt from 'markdown-it';
 
+const md = MarkdownIt();
 
-
-const md = MarkdownIt()
-
-async function fetchPosts(slug:any) {
-    const posts =  getAllPosts()
-    return posts.find((post) => post.slug === slug)
-    
+interface Post {
+    slug: string;
+    content: string;
+    title: string;
+    date: string;
+    [key: string]: any;
 }
 
-export default async function Post({params}:any) {
+async function fetchPosts(slug: string): Promise<Post | undefined> {
+    const posts = await getAllPosts();  // Fetch posts asynchronously
+    return posts.find((post) => post.slug === slug);  // Find the post by slug
+}
 
-    const post:any = await fetchPosts(params.slug)
+export default async function Post({ params }: { params: { slug: string } }) {
+    const post = await fetchPosts(params.slug);
 
+    if (!post) notFound();  // If no post found, return 404
 
-    if(!post) notFound()
+    const htmlConverter = md.render(post.content);  // Convert markdown to HTML
 
-    const htmlConverter = md.render(post.content)
-
-    return(
+    return (
         <article className="sm:max-w-4xl mx-auto sm:px-16 px-4">
-            <div className="prose font-[family-name:var(--font-geist-sans)] text-gray-800" dangerouslySetInnerHTML={{__html: htmlConverter}} />
+            <div
+                className="prose font-[family-name:var(--font-geist-sans)] text-gray-800"
+                dangerouslySetInnerHTML={{ __html: htmlConverter }}
+            />
             <h1>{post.title}</h1>
             <p>{post.date}</p>
         </article>
-    )
+    );
 }
