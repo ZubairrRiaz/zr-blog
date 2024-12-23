722c8209df762078b2a31f71edc09698470c6692
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-12-23 22:14:47 +0500

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.html", // Ensure to include HTML files if applicable
   ],
   
   theme: {
@@ -68,3 +67,4 @@ const config: Config = {
 };
 
 export default config;
+
